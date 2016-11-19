82b4c5df3664bf7b34875b193dbde546abbfdf54
rbens <rabah.benseghir@gmail.com>
2016-11-19 02:31:37 +0100

    Update gitignore file

--- src/main/java/com/rbens/controller/CreditDemoRestController.java
+++ src/main/java/com/rbens/controller/CreditDemoRestController.java
@@ -4,22 +4,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rbens.entity.Mensualite;
 import com.rbens.entity.Resultats;
 import com.rbens.entity.Series;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 @RequestMapping(value = "/")
 public class CreditDemoRestController {
 
 
-    @RequestMapping(value = "/amortissements", method = RequestMethod.POST)
+    @RequestMapping(value = "/amortissements", method = POST)
     public Mensualite getAmortissement(@RequestBody String mensualite) throws IOException {
         return new ObjectMapper().readValue(mensualite, Resultats.class);
     }
 
-    @RequestMapping(value = "/series", method = RequestMethod.POST)
+    @RequestMapping(value = "/series", method = POST)
     public Mensualite getSeries(@RequestBody String mensualite) throws IOException {
         return new ObjectMapper().readValue(mensualite, Series.class);
     }
---
+++ src/main/webapp/service/configService.js
@@ -0,0 +1,3 @@
+/**
+ * Created by rbens on 18.11.16.
+ */
