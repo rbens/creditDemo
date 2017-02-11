aa01dedc98d54639a0965b076c22553374287ff7
rbens <rabah.benseghir@gmail.com>
2017-02-11 16:32:35 +0100

    add video tuto

--- app/views/Content.js
+++ app/views/Content.js
@@ -1,8 +1,58 @@
-angular.module('mainApp').controller('content', function($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function() {
-        $mdSidenav('left').close()
-            .then(function(){
-                $log.debug("close LEFT is done");
-            });
+angular.module('mainApp').controller('content', function($scope, $window, $mdDialog) {
+    var showVideo = $window.localStorage.getItem('showVideo') || true;
+
+    $scope.openVideo =  function(ev){
+        $mdDialog.show({
+            parent: angular.element(document.body),
+            controller: DiaController,
+            templateUrl : 'views/modalTuto.html',
+            targetEvent:ev,
+            clickOutsideToClose:false
+        }).then(function(answer) {
+            $scope.status = 'cancel';
+        },function(){
+            $scope.status = 'close';
+        });
+
     };
+
+    angular.element(document).ready(function (ev) {
+        if(showVideo !== 'false'){
+            $mdDialog.show({
+                parent: angular.element(document.body),
+                controller: DiaController,
+                templateUrl : 'views/modalTuto.html',
+                targetEvent:ev,
+                clickOutsideToClose:false
+            }).then(function(answer) {
+                $scope.status = 'cancel';
+            },function(){
+                $scope.status = 'close';
+            });
+
+        }
+
+    });
+
 });
+
+function DiaController($scope,$window,$mdDialog) {
+    $scope.showVideo = $window.localStorage.getItem('showVideo') || false;
+
+    $scope.hide = function() {
+        $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+        $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+        $mdDialog.hide(answer);
+    };
+
+    $scope.onChange = function(value){
+        $window.localStorage.setItem('showVideo',value);
+    };
+}
+
