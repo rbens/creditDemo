865573141a3dcdfa6f5f9eb9c5ffded3ffa5eaba
rbens <rabah.benseghir@gmail.com>
2017-02-18 08:21:36 +0100

    jshint issue

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -49,7 +49,8 @@ module.exports = function (grunt) {
         jshint: {
             main: {
                 options: {
-                    jshintrc: '.jshintrc'
+                    jshintrc: '.jshintrc',
+                    reporterOutput: ""
                 },
                 src: createFolderGlobs('*.js')
             }
