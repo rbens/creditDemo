52ab30ad8dba95d25761a943296bd56a289146b1
rabah <rabah@debian-rb>
2017-10-16 13:59:57 +0200

    add tests

--- src/test/java/com/rbens/model/ResultsTest.java
+++ src/test/java/com/rbens/model/ResultsTest.java
@@ -1,12 +1,56 @@
 package com.rbens.model;
 
 import junit.framework.TestCase;
-import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  * Created by rabah on 15/10/17.
  */
 public class ResultsTest extends TestCase {
+    @Test
+    public void insuranceTotalCost() throws Exception {
+
+    }
+
+    @Test
+    public void interestTotalCost() throws Exception {
+
+    }
+
+    @Test
+    public void owingTotalCost() throws Exception {
+
+    }
+
+    @Test
+    public void creditTotalCost() throws Exception {
+
+    }
+
+    @Test
+    public void interetSeries() throws Exception {
+
+    }
+
+    @Test
+    public void assuranceSeries() throws Exception {
+
+    }
+
+    @Test
+    public void creditSeries() throws Exception {
+
+    }
+
+    @Test
+    public void capitalRestantSeries() throws Exception {
+
+    }
+
+    @Test
+    public void totalRestantSeries() throws Exception {
+
+    }
 
     private int months;
 
@@ -16,11 +60,14 @@ public class ResultsTest extends TestCase {
 
     private float insuranceRate;
 
-    @BeforeClass
+    //TODO : test must be finished
     public void setUp(){
         months = 12;
         capital = 15000;
         interestRate = 1.0f;
+    }
+
+    public void testInterestSeries(){
 
     }
 
