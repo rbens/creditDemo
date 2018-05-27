1cfeef66feff775ec102009f7c7f6874867b9dc0
rabah benseghir <rbens@MacBook-Pro-de-rabah.local>
2018-05-28 01:16:55 +0200

    add vendor

---
+++ app/vendor.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import "angular";
+import "@uirouter/angularjs";
+import "highcharts-ng";
+import "angular-strap";
+import "angular-material";
+import "angular-busy";
+import "angular-resource";
