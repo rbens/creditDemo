caa2c8712c5d2dd8de24e7a8964c42ad2ff3257b
rabah benseghir <rbens@MacBook-Pro-de-rabah.local>
2018-05-31 23:10:21 +0200

    remove grunt file

--- app/webpack.prod.js
+++ app/webpack.prod.js
@@ -1,13 +1,6 @@
 const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.base.js');
 
 module.exports = merge(common, {
-    mode: 'production',
-    devtool: 'source-map',
-    plugins: [
-         new UglifyJSPlugin({
-           sourceMap: true
-     })
-]
+    mode: 'production'
 });
