ca2b25f5f6cd193363f0161a0db44ed078dc3bbb
rbens <rabah.benseghir@gmail.com>
2018-06-10 18:30:54 +0200

    fix table header

--- app/components/tabResult/tabResult.controller.js
+++ app/components/tabResult/tabResult.controller.js
@@ -4,7 +4,7 @@ export default function tabResultController($scope,$filter, $document){
             $document.find('tbody').find('tr').remove();
             angular.forEach($scope.model.amortissements,function(val,key){
                 $document.find('tbody').append(
-                    '<tr class="tabLine"><td>'+val.currentMonth +' </td>'+
+                    '<tr class="tabLine"><td style="width: 10%">'+val.currentMonth +' </td>'+
                     '<td>'+$filter('number')(val.interestAmount,2)   +' € </td>'+
                     '<td>'+$filter('number')(val.principalAmount,2)  +' € </td>'+
                     '<td>'+$filter('number')(val.insuranceAmount,2)  +' € </td>'+
