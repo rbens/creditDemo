12ef429a962a6850552368c91193ce04290b7745
rbens <rabah.benseghir@gmail.com>
2018-06-12 00:47:26 +0200

    fix component bindings

--- app/components/tabResult/tabResult.controller.js
+++ app/components/tabResult/tabResult.controller.js
@@ -1,15 +1,17 @@
 export default function tabResultController($scope,$filter, $document){
     'ngInject';
-    $scope.$watch('$ctrl.model.model.amortissements',() => {
-        if(this.model.model.amortissements){
+    $scope.$watch('$ctrl.model.amortissements',() => {
+        if(this.model.amortissements){
             $document.find('tbody').find('tr').remove();
-            angular.forEach(this.model.model.amortissements,(val,key) => {
+            angular.forEach(this.model.amortissements,(val) => {
                 $document.find('tbody').append(
-                    '<tr class="tabLine"><td style="width: 10%">'+val.currentMonth +' </td>'+
-                    '<td>'+$filter('number')(val.interestAmount,2)   +' € </td>'+
-                    '<td>'+$filter('number')(val.principalAmount,2)  +' € </td>'+
-                    '<td>'+$filter('number')(val.insuranceAmount,2)  +' € </td>'+
-                    '<td>'+$filter('number')(val.owingAmount,2) +' € </td></tr>');
+                    `<tr class="tabLine">
+                     <td style="width: 10%">${val.currentMonth} </td>
+                     <td>${$filter('number')(val.interestAmount, 2)} € </td>
+                     <td>${$filter('number')(val.principalAmount, 2)} € </td>
+                     <td>${$filter('number')(val.insuranceAmount, 2)} € </td>
+                     <td>${$filter('number')(val.owingAmount, 2)} € </td>
+                     </tr>`);
             });
         }
     },true);
