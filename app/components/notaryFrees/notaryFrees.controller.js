6e8e095b03990cfb779e4f6ab0ba44f15c3029c7
rabah <rabah@debian-rb>
2019-04-15 08:54:00 +0200

    fix search city request

--- app/components/notaryFrees/notaryFrees.controller.js
+++ app/components/notaryFrees/notaryFrees.controller.js
@@ -2,7 +2,7 @@ import "../../service/cities/cities.service";
 
 function notaryFreesController($scope, cityService) {
 
-    let isNumber = (query) => query.match(new RegExp('\^[1-9]+'));
+    let isZipFormat = (query) => query.match(new RegExp('\^[1-9]+'));
     let self = $scope;
 
     self.notaryFreesInfo = {
@@ -10,15 +10,20 @@ function notaryFreesController($scope, cityService) {
         propertyType: '',
         zip: ''
     };
+
     self.propertiesType = ['ancien', 'neuf'];
-    let cityOrCodeFilter = (query, res) => isNumber(query) ? res.code.toString() : res.city.toLocaleLowerCase();
+
+    let cityOrCodeFilter = (query, res) => isZipFormat(query) ? res.code.toString() : res.city.toLocaleLowerCase();
+
     let applyQueryFilterOnCities = (query) => self.cities ? self.cities.then((res) => res.filter(value => cityOrCodeFilter(query,value).includes(query.toLocaleLowerCase()))) : [];
-    let initCities = (query) => self.cities = cityService.getCities(!isNumber(query) ? query : null, isNumber(query) ? query : null).then(
+
+    let initCities = (query) => self.cities = cityService.getCities(!isZipFormat(query) ? query : null, isZipFormat(query) ? query : null).then(
         // the service could not be available
         (res) =>  res.data.cities,
-        // so error is raised
+        // so error is raised and replace response by json file data
         () => cityService.loadCitiesFromJson().get().$promise.then((res) => res)
     );
+
     self.querySearch = (query) => query.length === 2 ?  initCities(query) : applyQueryFilterOnCities(query);
 
 }
