169c95da3c709f7455f16d7efedacba1e9545518
rabah <rabah.benseghir@gmail.com>
2019-05-11 00:21:41 +0200

    add puppeteer

--- app/karma.conf.js
+++ app/karma.conf.js
@@ -1,6 +1,7 @@
 // Karma configuration
 // Generated on Sat Nov 21 2015 16:55:19 GMT+0100 (CET)
 const webpackConfig = require('./webpack.test.js');
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
     config.set({
@@ -105,10 +106,6 @@ module.exports = function (config) {
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
         singleRun: true,
-
-        // Concurrency level
-        // how many browser should be started simultanous
-        concurrency: Infinity,
 
     });
 };
