22da9f99946200f039492833debad35f15e707ab
rabah <rabah.benseghir@gmail.com>
2019-05-10 23:46:27 +0200

    change karma chrome configuration

--- app/karma.conf.js
+++ app/karma.conf.js
@@ -73,6 +73,14 @@ module.exports = function (config) {
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['ChromeHeadless'],
+        browserDisconnectTimeout: 10000,
+        browserDisconnectTolerance: 3,
+        browserNoActivityTimeout: 60000,
+        flags: [
+            '--disable-web-security',
+            '--disable-gpu',
+            '--no-sandbox'
+        ],
 
         // Which plugins to enable
         plugins: [
