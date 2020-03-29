54f70bb727945feb326082109c73849b54fa1642
rabah <rabah.benseghir@gmail.com>
2020-03-29 19:25:49 +0200

    remove ng-annotate-loader , replace by angulrajs-annotate (compliant with babel)

--- app/webpack.base.js
+++ app/webpack.base.js
@@ -89,11 +89,11 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: [
-                    {loader: 'ng-annotate-loader'},
                     {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['@babel/preset-env']
+                            presets: ['@babel/preset-env'],
+                            plugins: ['angularjs-annotate']
                         }
                     }]
             },
